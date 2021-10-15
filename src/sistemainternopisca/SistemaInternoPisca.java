6b7fd6120716f7c7af5b906f5aca16051507a20f
Nickcarv18 <nicolicarvalho1@hotmail.com>
2021-10-15 19:41:36 -0300

    Commit initial

---
+++ src/sistemainternopisca/SistemaInternoPisca.java
@@ -0,0 +1,15 @@
+package sistemainternopisca;
+
+import UI_SistemaInterno.UI_Loading;
+
+public class SistemaInternoPisca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        UI_Loading loading = new UI_Loading();
+        loading.setVisible(true);
+    }
+    
+}
