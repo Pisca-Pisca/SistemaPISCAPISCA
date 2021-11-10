b7abd5bce8d0688d9a763a59480248341a1ea9bf
Nickcarv18 <nicolicarvalho1@hotmail.com>
2021-11-10 20:22:55 -0300

    Add tela de visualização de produto e funcionário

--- src/Model/DAO/ProdutosDAO.java
+++ src/Model/DAO/ProdutosDAO.java
@@ -13,8 +13,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
